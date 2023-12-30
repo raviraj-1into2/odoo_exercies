af0fd62709feaff3ea520f403d9e8066c7168ed1
Ravirajzala2703 <raviraj.reliution@gmail.com>
2023-12-30 23:09:15 +0530

    product import

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Product Import",
+    "version": "17.0.0.1",
+    "category": "",
+    "license": "LGPL-3",
+    "author": "Reliution",
+    "sequence": "1",
+    "description": """
+    """,
+    "depends": ['base','product'],
+    "data": [
+        "views/product_template_view.xml",
+    ],
+    "qweb": [],
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+}
